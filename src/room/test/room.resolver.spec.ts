5e67b9bc95ae011a3041684ff768aacc81bccb29
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-06-02 16:16:29 -0500

    test: unit tests

--- src/room/test/room.resolver.spec.ts
+++ src/room/test/room.resolver.spec.ts
@@ -1,18 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomResolver } from '../room.resolver';
+import { RoomService } from '../room.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Room, RoomDocument } from '../dto/room-schema';
+import { StateRoomEnum } from '../dto/state-room.enum';
 
 describe('RoomResolver', () => {
   let resolver: RoomResolver;
+  let roomService: RoomService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomResolver],
+      providers: [
+        RoomResolver,
+        RoomService,
+        {
+          provide: getModelToken(Room.name),
+          useValue: Room,
+        },
+      ],
     }).compile();
 
     resolver = module.get<RoomResolver>(RoomResolver);
+    roomService = module.get<RoomService>(RoomService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('getAllRooms', () => {
+    it('should return an array of rooms', async () => {
+      const mockRooms: RoomDocument[] = [
+        {
+          roomId: 'T120',
+          guestName: 'Guest One',
+          lastChanged: new Date(),
+          state: StateRoomEnum.FREE,
+        } as RoomDocument,
+        {
+          roomId: 'V230',
+          guestName: 'Guest Two',
+          lastChanged: new Date(),
+          state: StateRoomEnum.OCCUPIED,
+        } as RoomDocument,
+      ];
+      jest.spyOn(roomService, 'getAll').mockResolvedValue(mockRooms);
+
+      const result = await resolver.getAllRooms();
+
+      expect(result).toEqual(mockRooms);
+      expect(roomService.getAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('getOneRoom', () => {
+    it('should return a single room', async () => {
+      const roomId = 'T120';
+      const mockRoom: RoomDocument = {
+        roomId,
+        guestName: 'Gues Test',
+        lastChanged: new Date(),
+        state: StateRoomEnum.FREE,
+      } as RoomDocument;
+      jest.spyOn(roomService, 'getOne').mockResolvedValue(mockRoom);
+
+      const result = await resolver.getOneRoom(roomId);
+
+      expect(result).toEqual(mockRoom);
+      expect(roomService.getOne).toHaveBeenCalledWith(roomId);
+    });
   });
 });
--- src/room/test/room.service.spec.ts
+++ src/room/test/room.service.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomService } from '../room.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Room, RoomDocument } from '../dto/room-schema';
+import { StateRoomEnum } from '../dto/state-room.enum';
+import { NotFoundException } from '@nestjs/common';
 
 describe('RoomService', () => {
   let service: RoomService;
+  let roomModel: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomService],
+      providers: [
+        RoomService,
+        {
+          provide: getModelToken(Room.name),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            findOneAndUpdate: jest.fn(),
+            findOneAndRemove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<RoomService>(RoomService);
+    roomModel = module.get<Room>(getModelToken(Room.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array of rooms', async () => {
+      const mockRooms: RoomDocument[] = [
+        {
+          roomId: 'C212',
+          guestName: 'Guest One',
+          lastChanged: new Date(),
+          state: StateRoomEnum.FREE,
+        } as RoomDocument,
+        {
+          roomId: 'T234',
+          guestName: 'Guest Two',
+          lastChanged: new Date(),
+          state: StateRoomEnum.OCCUPIED,
+        } as RoomDocument,
+      ];
+      jest.spyOn(roomModel, 'find').mockResolvedValue(mockRooms);
+
+      const result = await service.getAll();
+
+      expect(result).toEqual(mockRooms);
+      expect(roomModel.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a single room', async () => {
+      const roomId = 'C212';
+      const mockRoom: RoomDocument = {
+        roomId,
+        guestName: 'Guest One',
+        lastChanged: new Date(),
+        state: StateRoomEnum.FREE,
+      } as RoomDocument;
+      jest.spyOn(roomModel, 'findOne').mockResolvedValue(mockRoom);
+
+      const result = await service.getOne(roomId);
+
+      expect(result).toEqual(mockRoom);
+      expect(roomModel.findOne).toHaveBeenCalledWith({ roomId });
+    });
+
+    it('should throw NotFoundException when room is not found', async () => {
+      const roomId = 'T679';
+      jest.spyOn(roomModel, 'findOne').mockResolvedValue(null);
+
+      await expect(service.getOne(roomId)).rejects.toThrow(NotFoundException);
+      expect(roomModel.findOne).toHaveBeenCalledWith({ roomId });
+    });
   });
 });
